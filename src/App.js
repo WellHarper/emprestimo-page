1fb9606dbd5ef472cdfd18409600f695f200dc0c
Wellerson <wellersonramos360@gmail.com>
2020-01-27 12:30:44 -0300

    cria formulario produtos

--- src/App.js
+++ src/App.js
@@ -45,38 +45,38 @@ function App() {
             <Row>
               <Col md={6}>
               <Form.Group>
-                <Form.Label>
+                <Form.Label className="lab">
                   CPF
                 </Form.Label>
-                <Form.Control type="text"/>
+                <Form.Control className="inputer" type="text"/>
               </Form.Group>
               </Col>
               <Col md={6}>
               <Form.Group>
-                <Form.Label>
+                <Form.Label className="lab">
                   CEP
                 </Form.Label>
-                <Form.Control type="text"/>
+                <Form.Control className="inputer"  type="text"/>
               </Form.Group>
               </Col>
             </Row>
             <Row>
               <Col>
                 <Form.Group>
-                  <Form.Label>
+                  <Form.Label className="lab">
                     NOME
                   </Form.Label>
-                  <Form.Control type="text"/>
+                  <Form.Control className="inputer"  type="text"/>
               </Form.Group>
               </Col>
             </Row>
             <Row>
               <Col md={6}>
               <Form.Group>
-                <Form.Label>
+                <Form.Label className="lab">
                   GÊNERO
                 </Form.Label>
-                <Form.Control as="select">
+                <Form.Control className="inputer"  as="select">
                   <option selected disabled>--Escolha--</option>
                   <option>Masculino</option>
                   <option>Feminino</option>
@@ -86,25 +86,46 @@ function App() {
               <Col md={6}>
               <Form.Group>
                   <Form.Label className="lab">DATA FINAL</Form.Label>
-                  <Form.Control type="date"/>
+                  <Form.Control className="inputer"  type="date"/>
                 </Form.Group>
               </Col>
             </Row>
             <Row>
               <Col>
-                <label>
+                <label className="lab">
                       CLIENTE JÁ FAZ PARTE DO CLUBE
                 </label>
                 </Col>
             </Row>
+        <Row>
+          <Col>
+           <span></span>
+            <span >CLIENTE DIAMANTE - SUPER SPOT CARTÃO</span>
+          </Col>
+        </Row>
+      </Form>
+        </Col>
+          <Col md={5} className="first-column">
             <Row>
               <Col>
-              <span></span>
-              <span>CLIENTE DIAMANTE - SUPER SPOT CARTÃO</span>
+              <Form.Label className="lab"> 
+                PRODUTO
+              </Form.Label>
+              <Form.Control className="inputer"  as="select">
+                <option selected disabled>--Escolha--</option>
+                <option>Cadeira de rodas</option>
+              </Form.Control>
               </Col>
             </Row>
-          </Form>
-        </Col>
+            <Row>
+              <Col className="prod">
+                <img className="cadeira" src="./cadeirante.png"/>
+              </Col>
+              <Col className="prod">
+                <img className="cadeira" src="./cadeirante.png"/>
+              </Col>
+            </Row>
+          </Col>
         </Row>
       </div>
       
